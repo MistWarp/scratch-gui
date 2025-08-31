35996f7286ea86d32932ac9814e380ba7ace7103
Mistium <d.a.kimber@icloud.com>
2025-08-31 20:36:33 +0100

    allow mistwarp embedding

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -41,12 +41,9 @@ import {isBrowserSupported} from '../lib/tw-environment-support-prober';
 import AddonChannels from '../addons/channels';
 import {loadServiceWorker} from './load-service-worker';
 import runAddons from '../addons/entry';
-import InvalidEmbed from '../components/tw-invalid-embed/invalid-embed.jsx';
 import {APP_NAME, FEEDBACK_URL, GITHUB_URL} from '../lib/brand.js';
 
 import styles from './interface.css';
-
-const isInvalidEmbed = window.parent !== window;
 
 // Import window manager dynamically
 let WindowManager = null;
@@ -294,10 +291,6 @@ class Interface extends React.Component {
         }
     }
     render () {
-        if (isInvalidEmbed) {
-            return <InvalidEmbed />;
-        }
-
         const {
             /* eslint-disable no-unused-vars */
             intl,
