9b47daa8398fab1229076b05e6f08be5dddfad92
Cubester <78769806+CubesterYT@users.noreply.github.com>
2025-11-20 21:42:35 -0500

    Completely rework monitor handling of Object and Arrays (#1113)

---
+++ src/lib/utils/tw-safe-stringify.js
@@ -0,0 +1,30 @@
+const circularReplacer = () => {
+    const seen = new WeakSet();
+    return (_, value) => {
+        if (typeof value === 'object' && value !== null) {
+            if (seen.has(value)) {
+                return Array.isArray(value) ? '[...]' : '{...}';
+            }
+            seen.add(value);
+        }
+        return value;
+    };
+};
+
+const sanitize = input => {
+    if (typeof input === 'object' && input !== null) {
+        return JSON.stringify(input, circularReplacer());
+    }
+    return input;
+    
+};
+
+const sanitizeVariableType = (input, type) => {
+    if (type === 'list') {
+        return input.map(item => sanitize(item));
+    }
+    return sanitize(input);
+  
+};
+
+export {sanitizeVariableType};
