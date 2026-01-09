42b4a2c23947114d085acde18b1f53f2eb9dd6ba
Mistium <d.a.kimber@icloud.com>
2026-01-09 15:58:59 +0000

    fix some minor bugs in the gui

--- src/components/gui/gui.jsx
+++ src/components/gui/gui.jsx
@@ -366,6 +366,7 @@ const GUIComponent = props => {
         onRequestCloseExtensionLibrary,
         onRequestCloseTelemetryModal,
         onSeeCommunity,
+        onSetStageSize: _onSetStageSize,
         onShare,
         onShowPrivacyPolicy,
         onStartSelectingFileUpload,
