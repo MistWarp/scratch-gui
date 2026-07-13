05da202cc8f666698721d9cfffbfad4eff5ef228
Mist <mist@mistium.com>
2026-07-13 19:51:40 +0100

    Build for dacf69b973654b87e371b65af47fbb5e6f7c7575

---
+++ js/gleba/addon-entry-tw-remove-feedback.9bd8f16465ef1f5f5662.js
@@ -0,0 +1,1 @@
+(window.webpackJsonpGUI=window.webpackJsonpGUI||[]).push([[64],{1978:function(n,e,s){"use strict";s.r(e),s.d(e,"resources",function(){return o});var c=s(1979);const o={"remove-feedback.css":s.n(c).a}},1979:function(n,e,s){(n.exports=s(13)(!1)).push([n.i,'a[class^="menu-bar_feedback-link_"] {\n  display: none;\n}\n',""])}}]);
