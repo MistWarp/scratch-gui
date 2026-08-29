9caa29ca031557b31785118a0af83dafe76b9dee
Mist <mist@mistium.com>
2026-08-29 20:21:36 +0100

    fix fonts

--- src/lib/tw-scratch-render-fonts/index.js
+++ src/lib/tw-scratch-render-fonts/index.js
@@ -7,21 +7,36 @@ const Marker = require('!!base64-loader!./Knewave.woff2');
 const Curly = require('!!base64-loader!./Griffy-Regular.woff2');
 const Pixel = require('!!base64-loader!./Grand9K-Pixel.woff2');
 const Scratch = require('!!base64-loader!./ScratchSavers_b2.woff2');
+const Playful = require('!!base64-loader!scratch-paint/node_modules/scratch-render-fonts/src/BadComic-Regular.ttf');
+const Bubbly = require('!!base64-loader!scratch-paint/node_modules/scratch-render-fonts/src/QTKooper.otf');
+const BitsAndBytes = require('!!base64-loader!scratch-paint/node_modules/scratch-render-fonts/src/freecam-v2.ttf');
+const Technological = require('!!base64-loader!scratch-paint/node_modules/scratch-render-fonts/src/MonospaceBold.ttf');
+const Arcade = require('!!base64-loader!scratch-paint/node_modules/scratch-render-fonts/src/PressStart2P.ttf');
+const Archivo = require('!!base64-loader!scratch-paint/node_modules/scratch-render-fonts/src/Archivo-Regular.ttf');
+const ArchivoBlack = require('!!base64-loader!scratch-paint/node_modules/scratch-render-fonts/src/Archivo-Black.ttf');
 
 const fontSource = {
-    'Sans Serif': SansSerif,
-    'Serif': Serif,
-    'Handwriting': Handwriting,
-    'Marker': Marker,
-    'Curly': Curly,
-    'Pixel': Pixel,
-    'Scratch': Scratch
+    'Sans Serif': {data: SansSerif, mime: 'font/woff2'},
+    'Serif': {data: Serif, mime: 'font/woff2'},
+    'Handwriting': {data: Handwriting, mime: 'font/woff2'},
+    'Marker': {data: Marker, mime: 'font/woff2'},
+    'Curly': {data: Curly, mime: 'font/woff2'},
+    'Pixel': {data: Pixel, mime: 'font/woff2'},
+    'Scratch': {data: Scratch, mime: 'font/woff2'},
+    'Playful': {data: Playful, mime: 'font/ttf'},
+    'Bubbly': {data: Bubbly, mime: 'font/otf'},
+    'Bits and Bytes': {data: BitsAndBytes, mime: 'font/ttf'},
+    'Technological': {data: Technological, mime: 'font/ttf'},
+    'Arcade': {data: Arcade, mime: 'font/ttf'},
+    'Archivo': {data: Archivo, mime: 'font/ttf'},
+    'Archivo Black': {data: ArchivoBlack, mime: 'font/ttf'}
 };
 
 const fontData = {};
 for (const fontName of Object.keys(fontSource)) {
+    const {data, mime} = fontSource[fontName];
     fontData[fontName] =
-        `@font-face{font-family:"${fontName}";src:url("data:font/woff2;base64,${fontSource[fontName]}");}`;
+        `@font-face{font-family:"${fontName}";src:url("data:${mime};base64,${data}");}`;
 }
 
 const addFontsToDocument = () => {
