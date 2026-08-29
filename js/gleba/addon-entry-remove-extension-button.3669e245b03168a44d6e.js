b2521b4bccfbb247291cba4a1f99933fe4693958
Mist <mist@mistium.com>
2026-08-29 17:32:06 +0100

    Build for d22062de0b4277d7d178dddfda8de32970b38fc6

---
+++ js/gleba/addon-entry-remove-extension-button.3669e245b03168a44d6e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonpGUI=window.webpackJsonpGUI||[]).push([[61],{2288:function(n,o,s){"use strict";s.r(o),s.d(o,"resources",function(){return e});var t=s(2289);const e={"hide-button.css":s.n(t).a}},2289:function(n,o,s){(n.exports=s(13)(!1)).push([n.i,'[class*="extension-button-container"] {\n  display: none !important;\n}',""])}}]);
