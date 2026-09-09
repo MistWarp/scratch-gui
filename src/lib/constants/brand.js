280883b16cbb2e299058c2249683d527e39f7051
Mist <mist@mistium.com>
2026-09-09 08:00:16 +0100

    change feedback url

--- src/lib/constants/brand.js
+++ src/lib/constants/brand.js
@@ -1,5 +1,5 @@
 const APP_NAME = 'MistWarp';
-const FEEDBACK_URL = 'https://scratch.mit.edu/users/m1stium#comments';
+const FEEDBACK_URL = '/roadmap';
 const GITHUB_URL = 'https://github.com/MistWarp';
 
 export {APP_NAME, FEEDBACK_URL, GITHUB_URL};
